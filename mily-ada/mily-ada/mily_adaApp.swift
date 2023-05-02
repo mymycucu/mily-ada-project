5fa64664c15cfe2294469ada561ecf4bceb2eb97
Hilmy Noerfatih <93917960+mymycucu@users.noreply.github.com>
2023-05-02 10:44:14 +0700

    setup git ignore

---
+++ mily-ada/mily-ada/mily_adaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mily_adaApp.swift
+//  mily-ada
+//
+//  Created by Hilmy Noerfatih on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct mily_adaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
